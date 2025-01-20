b1e02fe1cad0fd4a7482ee77dcc537c27a5054d0
Bolaji06 <joshspencer0603@gmail.com>
2025-01-20 22:00:02 +0100

    converts contents to json and markdown format

--- document-parser/src/index.js
+++ document-parser/src/index.js
@@ -1,3 +1,11 @@
-import PDFParser from "./parsers/pdfParser";
+import PDFParser from "./parsers/pdfParser.js";
 
-export { PDFParser }
+(async function main(){
+  const pdfParser = new PDFParser("../sample/sample.pdf");
+  const parser = await pdfParser.parse([1]);
+  //console.log(pdfParser.json(parser.text));
+  console.log(pdfParser.markdown(parser.text))
+
+
+})().catch(error => console.log(error))
+//export { PDFParser }
