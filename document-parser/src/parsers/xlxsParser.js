842924de8c4bd24c3a1aa56f023e0f9f7eec5d4b
Bolaji06 <joshspencer0603@gmail.com>
2025-01-21 00:57:34 +0100

    implement xlxs parser

---
+++ document-parser/src/parsers/xlxsParser.js
@@ -0,0 +1,35 @@
+import * as XLSX from "xlsx";
+import { readFileSync } from "fs";
+
+class XlsxParser {
+  constructor(filePath) {
+    this.filePath = filePath;
+    if (!filePath) {
+      throw new Error("File path is required for initialization");
+    }
+  }
+  async parser() {
+    try {
+      const buf = readFileSync(this.filePath);
+      //const textContent = buf.toString()
+      const workbook = XLSX.read(buf);
+      const sheets = workbook.Sheets;
+      const sheetNames = workbook.SheetNames;
+
+      //return XLSX.utils.sheet_to_txt(sheets['Form Responses 1'])
+
+      return sheetNames.map((sheet) => {
+        const rawText = XLSX.utils.sheet_to_txt(sheets[sheet]);
+        const cleanedText = rawText.replace(/\x00/g, ""); // Remove null characters
+        return {
+          sheet,
+          workspace: cleanedText,
+        };
+      });
+    } catch (error) {
+      throw new Error("Error: " + error.message);
+    }
+  }
+}
+
+export default XlsxParser;
